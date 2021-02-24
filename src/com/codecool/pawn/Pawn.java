dc82a3df46d8e50940ac9c1d8baf5c9604a571cd
unknown <lok3rsss@gmail.com>
2021-02-24 14:00:40 +0100

    add isMoveInBounds validation for Pawn object

--- src/com/codecool/pawn/Pawn.java
+++ src/com/codecool/pawn/Pawn.java
@@ -1,4 +1,43 @@
 package com.codecool.pawn;
 
 public class Pawn {
+
+    int positionX;
+    int positionY;
+    boolean isWhite;
+
+    public Pawn(int positionX, int positionY, boolean isWhite){
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.isWhite = isWhite;
+    }
+
+    public static void validateMove(char[][] gameBoard, int targetX, int targetY){
+
+    }
+
+    private boolean isMoveInBounds(char[][] gameBoard, int targetX, int targetY){
+        return gameBoard.length > targetY && gameBoard[targetY].length > targetX;
+    }
+
+    public void setPositionX(int positionX){
+        this.positionX = positionX;
+    }
+
+    public void setPositionY(int positionY){
+        this.positionY = positionY;
+    }
+
+    public boolean getIsWhite(){
+        return isWhite;
+    }
+
+    public int getPositionX(){
+        return positionX;
+    }
+    public int getPositionY(){
+        return positionY;
+    }
+
+
 }
