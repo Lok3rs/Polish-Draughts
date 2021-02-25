b83e034ad0c122953ea36d6f14ef11e66971677a
kasluk345 <kaszuba.lukasz@gmail.com>
2021-02-25 20:23:17 +0100

    printBoard method implemented

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -8,7 +8,9 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        Board newBoard = new Board(7);
+        Board newBoard = new Board(8);
         System.out.println(Arrays.deepToString(newBoard.initBoard()));
+        newBoard.initBoard();
+        newBoard.printBoard();
     }
 }
--- src/com/codecool/board/Board.java
+++ src/com/codecool/board/Board.java
@@ -5,13 +5,14 @@ import com.codecool.pawn.Pawn;
 
 public class Board {
 
-    int size;
+    public int size;
+    public Pawn[][] gameBoard;
 
-    public Board(int size){
+    public Board(int size) {
         this.size = size;
     }
 
-    public Pawn[][] initBoard(){
+    public Pawn[][] initBoard() {
         Pawn[][] gameBoard = new Pawn[size][size];
         return fillBoard(gameBoard);
     }
@@ -21,13 +22,44 @@ public class Board {
         int beforeLastIndex = gameBoard.length - 2;
 
         for (int i = 0; i < gameBoard.length; i++) {
-            for (int j = 0; j < gameBoard.length; j++){
-                if (i == 0) { gameBoard[i][j] = j % 2 == 0 ? new Pawn(i, j, false) : null; }
-                else if (i == 1) { gameBoard[i][j] = j % 2 == 1 ? new Pawn(i, j, false) : null; }
-                else if (i == lastIndex) { gameBoard[i][j] = j % 2 == 0 ? new Pawn(i, j, true) : null; }
-                else if (i == beforeLastIndex ) { gameBoard[i][j] = j % 2 == 1 ? new Pawn(i, j, true) : null; }
+            for (int j = 0; j < gameBoard.length; j++) {
+                if (i == 0) {
+                    gameBoard[i][j] = j % 2 == 0 ? new Pawn(i, j, false) : null;
+                } else if (i == 1) {
+                    gameBoard[i][j] = j % 2 == 1 ? new Pawn(i, j, false) : null;
+                } else if (i == lastIndex) {
+                    gameBoard[i][j] = j % 2 == 0 ? new Pawn(i, j, true) : null;
+                } else if (i == beforeLastIndex) {
+                    gameBoard[i][j] = j % 2 == 1 ? new Pawn(i, j, true) : null;
                 }
             }
+        }
         return gameBoard;
     }
+
+    public void printBoard() {
+
+        Pawn [][] boardToPrint = initBoard();
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < size; j++) {
+                if (boardToPrint[i][j] == null) {
+                    System.out.print("-");
+                } else {
+                    if (boardToPrint[i][j].getIsWhite() == true) {
+                        System.out.print("W");
+                    }
+                    if (boardToPrint[i][j].getIsWhite() == false) {
+                        System.out.print("B");
+
+                    }
+
+                }
+
+            }
+        }
+    }
 }
+
+
