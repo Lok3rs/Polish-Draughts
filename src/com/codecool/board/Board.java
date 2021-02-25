7224f0721618cbbd491a4bc8cd8a672f9822305e
unknown <lok3rsss@gmail.com>
2021-02-25 19:37:04 +0100

    Add board initialization

--- src/com/codecool/board/Board.java
+++ src/com/codecool/board/Board.java
@@ -1,5 +1,33 @@
 package com.codecool.board;
 
 
+import com.codecool.pawn.Pawn;
+
 public class Board {
+
+    int size;
+
+    public Board(int size){
+        this.size = size;
+    }
+
+    public Pawn[][] initBoard(){
+        Pawn[][] gameBoard = new Pawn[size][size];
+        return fillBoard(gameBoard);
+    }
+
+    public Pawn[][] fillBoard(Pawn[][] gameBoard) {
+        int lastIndex = gameBoard.length - 1;
+        int beforeLastIndex = gameBoard.length - 2;
+
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard.length; j++){
+                if (i == 0) { gameBoard[i][j] = j % 2 == 0 ? new Pawn(i, j, false) : null; }
+                else if (i == 1) { gameBoard[i][j] = j % 2 == 1 ? new Pawn(i, j, false) : null; }
+                else if (i == lastIndex) { gameBoard[i][j] = j % 2 == 0 ? new Pawn(i, j, true) : null; }
+                else if (i == beforeLastIndex ) { gameBoard[i][j] = j % 2 == 1 ? new Pawn(i, j, true) : null; }
+                }
+            }
+        return gameBoard;
+    }
 }
